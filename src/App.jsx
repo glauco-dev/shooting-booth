3d100d7c5f2e104920d37f03b8795a746cb9f68a
glauco-dev <icaroglauco@email.com>
2024-07-05 16:08:02 -0300

    Já desenvolvendo a navegação In app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { Box, Button } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Button, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 export default () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+
     return (
         <Box>
-            <Button />
+            <Button ref={btnRef} bg="transparent" right={0} onClick={onOpen}>
+                <HamburgerIcon />
+            </Button>
         </Box>
     );
 }
